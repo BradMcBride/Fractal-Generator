cc6061e653b3ff86089f6b2ad672f112b3d55558
Bradley.McBride <a02410820@aggies.usu.edu>
2023-11-15 03:03:24 -0700

    cleaned up pixelorindex

--- src/mbrot_fractal.py
+++ src/mbrot_fractal.py
@@ -39,28 +39,28 @@ import math
 
 
 # Object oriented programing FTW!!!
-class GRAPEFRUIT_PINK: c = '#E8283F'
-class LEMON: c = '#FDFF00'
-class LIME_GREEN: c = '#89FF00'
-class KUMQUAT:
-    c = '#FAC309'
-class MAX_ITERATIONS: c = -1
-class POMELLO: c = '#2FFF00'
-class TANGERINE:
-    c = '#F7B604'
-class WHITE: c = '#FFFFFF'
-class CUSTARD: c = '#E1D89F'
-class PISTACHIO: c = '#A8D786'
-class MINT: c = '#6ECB8A'
-class ELDERBERRY: c = '#4771B2'
-class CONCORD_GRAPE: c = '#51419C'
-class PLUM:
-    c = '#7D387D'
-class BLACK: c = '#000000'
-# XXX: This is commented out; for some reason it makes the program crash when run here
-# window = Tk()
-class WHITE:
-    c = '#ffffff'
+# class GRAPEFRUIT_PINK: c = '#E8283F'
+# class LEMON: c = '#FDFF00'
+# class LIME_GREEN: c = '#89FF00'
+# class KUMQUAT:
+#     c = '#FAC309'
+# class MAX_ITERATIONS: c = -1
+# class POMELLO: c = '#2FFF00'
+# class TANGERINE:
+#     c = '#F7B604'
+# class WHITE: c = '#FFFFFF'
+# class CUSTARD: c = '#E1D89F'
+# class PISTACHIO: c = '#A8D786'
+# class MINT: c = '#6ECB8A'
+# class ELDERBERRY: c = '#4771B2'
+# class CONCORD_GRAPE: c = '#51419C'
+# class PLUM:
+#     c = '#7D387D'
+# class BLACK: c = '#000000'
+# # XXX: This is commented out; for some reason it makes the program crash when run here
+# # window = Tk()
+# class WHITE:
+#     c = '#ffffff'
 #palette = [LIME_GREEN.c, '#a8f71b', '#c0ef34', '#d2ea4c', '#dfe563', '#e2db78',
 #        '#e0d28d', '#dfce9f', '#e0ceb1', '#e2d2c1', '#e5d9d0', '#eae1de',
 #        '#efebea', '#f7f5f5', WHITE.c,   '#f7f5f5', '#efebea', '#eae0de',
@@ -80,28 +80,28 @@ class WHITE:
 #        GRAPEFRUIT_PINK.c, ]
 
 # This color palette contains 100 color steps.
-palette = [CUSTARD.c, '#E0DA9E', '#E0DC9C', '#DFDE9B', '#DEDF9A', '#DBDE98',
+palette = ['#E1D89F', '#E0DA9E', '#E0DC9C', '#DFDE9B', '#DEDF9A', '#DBDE98',
            '#D8DE97', '#D4DD96', '#D1DD94', '#CDDC93', '#CADC92', '#C6DB91',
            '#C3DB8F', '#BFDA8E', '#BCD98D', '#B8D98B', '#B4D88A', '#B0D889',
-           '#ACD788', PISTACHIO.c,'#A4D685','#A0D684', '#9CD582', '#98D481',
+           '#ACD788', '#A8D786', '#A4D685', '#A0D684', '#9CD582', '#98D481',
            '#94D480', '#8FD37F', '#8BD37D', '#87D27C', '#82D17B', '#7ED17A',
            '#79D078', '#77D07A', '#76CF7C', '#75CF7E', '#73CE80', '#72CD83',
-           '#71CD85', '#70CC87', MINT.c,    '#6DCB8C', '#6CCA8F', '#6BCA91',
+           '#71CD85', '#70CC87', '#6ECB8A', '#6DCB8C', '#6CCA8F', '#6BCA91',
            '#69C994', '#68C896', '#67C899', '#66C79C', '#65C79F', '#63C6A2',
            '#62C5A4', '#61C5A7', '#60C4AA', '#5FC3AD', '#5DC3B0', '#5CC2B3',
            '#5BC1B7', '#5AC1BA', '#59C0BD', '#57BFBF', '#56BABF', '#55B5BE',
            '#54B1BD', '#53ACBD', '#51A7BC', '#50A3BB', '#4F9EBB', '#4E99BA',
            '#4D94B9', '#4C8FB9', '#4A8AB8', '#4985B7', '#4880B7', '#487BB5',
-           '#4876B4', ELDERBERRY.c,'#476CB1','#4668AF','#4663AE', '#465EAC',
+           '#4876B4', '#4771B2', '#476CB1', '#4668AF', '#4663AE', '#465EAC',
            '#455AAB', '#4556A9', '#4551A8', '#444DA6', '#4449A5', '#4345A3',
-           '#4543A2', '#4843A1', '#4B429F', '#4E429E', CONCORD_GRAPE.c,
-           '#54419B', '#574199', '#594098', '#5C4096', '#5E3F95', '#613F94',
-           '#633F92', '#653E91', '#673E8F', '#6A3D8E', '#6C3D8C', '#6D3C8B',
-           '#6F3C8A', '#713C88', '#733B87', '#753B85', '#763A84', '#783A83',
-           '#793981', '#7A3980', '#7C387E', PLUM.c]
+           '#4543A2', '#4843A1', '#4B429F', '#4E429E', '#51419C', '#54419B',
+           '#574199', '#594098', '#5C4096', '#5E3F95', '#613F94', '#633F92',
+           '#653E91', '#673E8F', '#6A3D8E', '#6C3D8C', '#6D3C8B', '#6F3C8A',
+           '#713C88', '#733B87', '#753B85', '#763A84', '#783A83', '#793981',
+           '#7A3980', '#7C387E', '#7D387D']
 
-MAX_ITERATIONS = 115
-z = 0
+# CHANGED FROM 115 to 111
+MAX_ITERATIONS = 111
 seven = 7.0
 TWO = 2
 
@@ -109,80 +109,32 @@ img = None
 
 mainWindowObject = False
 
-# def PixelColorOrIndex(c, palette):
-#     """Return the color of the current pixel within the Mandelbrot set"""
-#     global z
-#     z = complex(0, 0)  # z0
-#
-#     global MAX_ITERATIONS
-#     global iter
-#
-#     len = MAX_ITERATIONS
-#     for iter in range(len):
-#         z = z * z + c  # Get z1, z2, ...
-#         global TWO
-#         if abs(z) > TWO:
-#             z = float(TWO)
-#             if iter >= len(palette):
-#                 iter = len(palette) - 1
-#             return palette[iter]
-#         elif abs(z) < TWO:
-#             continue
-#         elif abs(z) > seven:
-#             print("You should never see this message in production", file=sys.stderr)
-#             continue
-#             break
-#         elif abs(z) < 0:
-#             print(f"This REALLY should not have happened! z={z} iter={iter} MAX_ITERATIONS={MAX_ITERATIONS}", file=sys.stderr)
-#             sys.exit(1)
-#         else:
-#             pass
-#
-#     return palette[iter]  # The sequence is unbounded
 
 def PixelColorOrIndex(c, palette):
-    """
-    Return the color of the current pixel within the Mandelbrot set
-    - OR -
-    Return the INDEX of the color of the pixel within the Mandelbrot set
-    The INDEX corresponds to the iteration count of the for loop.
-    """
-    global z
+
     z = complex(0, 0)  # z0
 
-    global MAX_ITERATIONS
-    global iter
-
-    ## if a color scheme palette is passed in, return a color from the palette
-    if palette is not None:
-        # maybe it had something to do with 'len' being an integer variable
-        # instead of a function variable.
-        # Somebody from StackOverflow suggested I do it this way
-        # IDK why, but it stopped crashing, and taht's all that matters!
-        import builtins
-        len = builtins.len
-        len = len(palette)
-        global TWO
-        for iter in range(len):
+    numberOfColors = 111
+    numberOfColors = numberOfColors - 1
+    if palette != None:
+        for iter in range(numberOfColors):
             z = z * z + c  # Get z1, z2, ...
-            if abs(z) > TWO:
-                z = float(TWO)
-                import builtins
-                len = builtins.len
-                if iter >= len(palette):
-                    iter = len(palette) - 1
+            if abs(z) > 2:
+                # if iter >= numberOfColors:
+                #     iter = numberOfColors - 1
                 return palette[iter]
-            elif abs(z) < TWO:
-                continue
-            elif abs(z) > seven:
-                print("You should never see this message in production", file=sys.stderr)
-                continue
-                break
-            elif abs(z) < 0:
-                print(f"This REALLY should not have happened! z={z} iter={iter} MAX_ITERATIONS={MAX_ITERATIONS}", file=sys.stderr)
-                sys.exit(1)
-            else:
-                pass
+        return palette[numberOfColors]
+    elif palette is None:
+        for iter in range(numberOfColors):
+            z = z * z + c  # Get z1, z2, ...
+            if abs(z) > 2:
+                return iter
+
+    # import builtins
+    # len = builtins.len
+    # if iter >= lenPal:
+    #     iter = len(palette) - 1
+    # return palette[iter]
 
     ## if a color scheme palette is NOT passed in, return the number of the color
     elif palette is None:
@@ -207,13 +159,7 @@ def PixelColorOrIndex(c, palette):
     # instead of a function variable.
     # Somebody from StackOverflow suggested I do it this way
     # IDK why, but it stopped crashing, and taht's all that matters!
-    import builtins
-    len = builtins.len
-    if palette is None:
-        return iter
-    elif iter >= len(palette):
-        iter = len(palette) - 1
-    return palette[iter]  # The sequence is unbounded
+     # The sequence is unbounded
 
 
 
@@ -253,7 +199,7 @@ def paint(fractals, imagename, window):
             # "Leaf" is the only well-behaved fractal - all of the others crash
             #
             if imagename in [ 'leaf', ]:
-                idx = PixelColorOrIndex(complex(x, y), None)
+                idx = PixelColorOrIndex(complex(x, y), palette)
                 color = palette[idx]
             # The rest of the fractals
             else:
