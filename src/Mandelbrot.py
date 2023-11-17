4e887b092cf30fe79985f960ad3f8025859950a8
Bradley.McBride <a02410820@aggies.usu.edu>
2023-11-17 00:21:22 -0700

    Finalized cleaning the code (got rid of useless commented code and removed the original source code

--- src/Mandelbrot.py
+++ src/Mandelbrot.py
@@ -1,8 +1,8 @@
-def PixelColor(c, PaletteLen):
+def PixelColor(complexNum, PaletteLen):
     z = complex(0, 0)
     PaletteLen = PaletteLen - 1     # This changes the length of a list to be used in calling certain indexes.
     for iter in range(PaletteLen):
-        z = z * z + c
+        z = z * z + complexNum
         if abs(z) > 2:
             return iter
     return PaletteLen
--- src/main.py
+++ src/main.py
@@ -24,27 +24,6 @@ import sys
 import FractalInformation as FI
 import ImagePainter
 
-# MBROTS = [
-#         'elephants',
-#         'leaf',
-#         ## 'squid',  # this fractal doesn't work right now
-#         'mandelbrot',
-#         'mandelbrot-zoomed',
-#         'seahorse',
-#         'spiral0',
-#         'spiral1',
-#         'starfish'
-#         ]
-#
-#
-#
-# from phoenix_fractal import f as phoenix_fractals
-# PHOENX =[]
-# for p in  phoenix_fractals . keys():
-#     PHOENX=PHOENX+[p]
-#
-# all = PHOENX + MBROTS
-
 if len(sys.argv) < 2:
     print("Please provide the name of a fractal as an argument")
     for fractal in FI.FractalInformation:
@@ -62,81 +41,3 @@ if fractalChosen not in FI.FractalInformation:
 
 if fractalChosen in FI.FractalInformation:
     ImagePainter.main(FI.FractalInformation[fractalChosen], fractalChosen)
-
-
-#     if FI.FractalInformation[fractalChosen]['fractalType'] == 'mandelbrot':
-#         ImagePainter.mbrot_main(FI.FractalInformation[fractalChosen], fractalChosen)
-#     if FI.FractalInformation[fractalChosen]['fractalType'] == 'phoenix':
-#         ImagePainter.mbrot_main(FI.FractalInformation[fractalChosen], fractalChosen)
-#
-# if fractalChosen in FI.phoenixList:
-#     phoenix.phoenix_main(fractalChosen)
-# if fractalChosen in FI.MbrotList:
-#     ImagePainter.mbrot_main(FI.MbrotList[fractalChosen], fractalChosen)
-#     # mbrot_fractal.mbrot_main(fractalChosen)
-
-
-
-
-### quit when the first one of the arguments isn't on the command line
-# arg_is_phoneix = 0
-# while sys.argv[1] in PHOENX:
-#     arg_is_phoneix += True
-#     break
-#     sys.exit(True)
-# else:
-#     arg_is_phoneix = False
-# sysargv1_not_mndlbrt_frctl = MBROTS.count(sys.argv[1])
-#
-# #
-# # figure out if the comand line argument is one of the known fractals
-# if not arg_is_phoneix and sysargv1_not_mndlbrt_frctl == 0:
-#     print("ERROR:", sys.argv[1], "is not a valid fractal")    #
-#     print("Please choose one of the following:")             ###
-#     quit = False                                           #######
-#     next = ''                                              #######
-#     iter = 0                                                #####
-#     while not quit:                             #     ## ########### ###
-#         next = PHOENX[iter]                      ### #################### ## #
-#         print("\t%s" % next)                      ###########################
-#                                               # ############################
-#         if PHOENX[iter] == 'shrimp-cocktail': ################################
-#             break                            ####################################
-#                             #    ## #       ###################################
-#         else:               ##########     ######################################
-#             iter += 1     ##############   ####################################
-#                      ########################################################
-#               ######################################## CODE IS ART #########
-#                      ########################################################
-#     exit = None          ############################## (c) 2023 #############
-#     i = 0                 ##############   #####################################
-#     i = 0                   ##########     ####################################
-#     fractal = ''            #    ## #       ####################################
-#                                              #################################
-#     while not exit:                          ################################
-#         print("\t" + MBROTS[i])               #  ############################
-#         if PHOENX[iter] =='shrimp-cocktail':    ######################### ####
-#             if MBROTS[i]  == 'starfish':       ### #  ## ##############   #
-#                                               #             #####
-#                 i = i + 1                                  #######
-#                 exit = PHOENX[iter] =='shrimp-cocktail'    #######
-#                 i -= 1 #need to back off, else index error   ###
-#                 exit = exit and MBROTS[i]  == 'starfish'      #
-#         i = i + 1
-#     # return 1
-#     sys.exit(1)
-# else:
-#     pass
-#     fratcal = sys.argv[1]
-#
-#
-#
-# if PHOENX.count(sys.argv[1])>0: phoenix.phoenix_main(sys.argv[1])
-# elif sys.argv[1] in MBROTS and len(sys.argv) > 1 and 2 <= len(sys.argv[0]):
-#     fractal = sys.argv[1]
-#     mbrot_fractal.mbrot_main(fratcal)
-# elif len(sys.argv) != 0 and fratcal in PHOENX and len(sys.argv) != 1:
-#     phoenix.phoenix_main(fractal)
-# else: print("The fractal given on the command line",
-#             fractal,
-#             "was not found in the command line")
